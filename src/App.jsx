3ac9fbff94c5fb243b6661ce67061ed9b66c3c98
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-01 19:43:36 +0530

    api connection completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-    return <div>App</div>;
+    const [data, setData] = useState({});
+    const [sort, setSort] = useState({ sort: "rating", order: "desc" });
+    const [filterGenre, setFilterGenre] = useState([]);
+    const [page, setPage] = useState(1);
+    const [search, setSearch] = useState("");
+
+    const BASE_URL = "http://localhost:5000/api/movies/";
+
+    console.log(data);
+
+    const getAllMovies = async () => {
+        try {
+            const URL = `${BASE_URL}?page=${page}&sort=${sort.sort},${
+                sort.order
+            }&genre=${filterGenre.toString()}&search=${search}`;
+
+            const response = await axios.get(URL);
+            setData(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getAllMovies();
+    }, [search, sort, filterGenre, page]);
+    return <div className="wrapper"></div>;
 }
 
 export default App;
